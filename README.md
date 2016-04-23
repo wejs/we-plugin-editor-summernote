d12a7bd734f655287a386bd0353a8ff32ed94a64
albertosouza <alberto.souza.99@gmail.com>
2016-04-22 22:29:39 -0300

    init with code from wejs/we-core ref wejs/we#138

---
+++ README.md
@@ -0,0 +1,21 @@
+# we-plugin-editor-summernote
+
+> Add the html form field and summernote features to client site
+
+Incompatible with others npm modules starting with: we-plugin-editor-* plugins
+
+## Ìnstallation
+
+In project folder tip:
+
+```sh
+npm install we-plugin-editor-summernote --save
+```
+
+## Links
+
+> * We.js site: http://wejs.org
+
+## license
+
+under [the MIT license](LICENSE).
---
+++ plugin.js
@@ -0,0 +1,41 @@
+/**
+ * We.js summernote plugin main file
+ *
+ * see http://wejs.org/docs/we/plugin
+ */
+module.exports = function loadPlugin(projectPath, Plugin) {
+  var plugin = new Plugin(__dirname);
+
+  plugin.addJs('summernote', {
+    weight: 5, pluginName: 'we-plugin-editor-summernote',
+    path: 'files/public/summernote/dist/summernote.js'
+  });
+  plugin.addCss('summernote', {
+    weight: 5, pluginName: 'we-plugin-editor-summernote',
+    path: 'files/public/summernote/dist/summernote.css'
+  });
+  plugin.addJs('summernote-ext-video', {
+    weight: 7, pluginName: 'we-plugin-editor-summernote',
+    path: 'files/public/summernote/plugin/summernote-ext-video.js'
+  });
+  plugin.addCss('summernote-bs3', {
+    weight: 6, pluginName: 'we-plugin-editor-summernote',
+    path: 'files/public/summernote/dist/summernote-bs3.css'
+  });
+  plugin.addJs('we-plugin-editor-summernote', {
+    weight: 11, pluginName: 'we-plugin-editor-summernote',
+    path: 'files/public/we-plugin-editor-summernote.js'
+  });
+
+  plugin.events.on('render-javascript-tags:before:render', function (data) {
+    // add locale file for non en-us locales
+    if (data.context.locale && data.context.locale !== 'en-us') {
+      data.files.push(
+        '/public/plugin/we-plugin-editor-summernote/files/summernote/lang/summernote-'+
+        data.context.locale + '.js'
+      );
+    }
+  });
+
+  return plugin;
+};
